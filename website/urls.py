a7bf7f12f66cd63cda65604d48f4568f9f93c1f5
GSinseswa721 <glorianiyonkurusinseswa@gmail.com>
2022-02-21 20:21:28 -0800

    added pages

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from. import views
 
 urlpatterns = [
 path('', views.home, name="home"),
-
-path('shop_grid_3_columns_sidebar.html', views.shop-grid-3-columns-sidebar, name="shop_grid_3_columns_sidebar")
-
+path('about-us.html', views.about_us, name="about_us"),
+path('contact-us.html', views.contact_us, name="contact_us"),
+path('track-order.html',views.track_order, name="track_order")
 ]
--- website/views.py
+++ website/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {})
 
-def shopgrid_3_columns_sidebar(request):
-    return render(request, 'shop_grid_3_columns_sidebar.html', {})
+def about_us(request):
+    return render(request, 'about-us.html', {})
+def contact_us(request):
+    return render(request, 'contact-us.html', {})
+def track_order(request):
+    return render(request, 'track-order.html', {})
 
